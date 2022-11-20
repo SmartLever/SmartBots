7ff92244adf8c01cebe0fe1245cb0a729b975f4d
jose <jacarpio17@gmail.com>
2022-11-20 23:35:19 +0100

    update portfolio_production

--- src/application/bots/portfolio_production.py
+++ src/application/bots/portfolio_production.py
@@ -22,12 +22,32 @@ def main(run_real: bool = False, send_orders_to_broker: bool = True,
     """ Run the portfolio engine. """
     from src.domain.config_helper import get_config
     import os
+    import pandas as pd
     from src.application import conf
     from src.application.services.portfolio_constructor import Portfolio_Constructor
-    path_to_config = os.path.join(conf.path_to_data,"config_portfolios", conf_portfolio+".yaml")
+    path = os.path.abspath(__file__)
+    path_bot = os.path.dirname(path)  # path to the module
+    path_to_config = os.path.join(conf.path_to_data, "config_portfolios", conf_portfolio+".yaml")
     conf_portfolio = get_config(path_to_config)  # get the configuration from the config file
-    # run the portfolio engine, set run_real=True to run in real time
+    # if our strategy is in a csv file
+    list_strategy = []
+    if conf_portfolio['Strategies_Load_From']['from']:
+        # load strategy from csv
+        path_info_strategy = os.path.join(path_bot, f'{asset_type}_trading', conf_portfolio['Strategies_Load_From']['from'])
+        info_strategy = pd.read_csv(path_info_strategy)
+        for index, strategy in info_strategy.iterrows():
+            dict_strategy = {'params': {}}
+            for k in strategy.keys():
+                if k == 'id':
+                    dict_strategy['id'] = strategy[k]
+                elif k == 'name':
+                    dict_strategy['strategy'] = strategy[k]
+                else:
+                    dict_strategy['params'][k] = strategy[k]
+            list_strategy.append(dict_strategy)
+        conf_portfolio['Strategies'] = list_strategy
 
+    # run the portfolio engine, set run_real=True to run in real time
     portfolio_production = Portfolio_Constructor(conf_portfolio, run_real=run_real, asset_type=asset_type,
                                                  send_orders_to_broker=send_orders_to_broker, start_date=start_date)
     portfolio_production.run()
@@ -36,7 +56,7 @@ def main(run_real: bool = False, send_orders_to_broker: bool = True,
 if __name__ == '__main__':
     run_real = True
     asset_type = 'financial'
-    conf_portfolio = 'config_webhook'
+    conf_portfolio = 'config_financial'
     start_date = dt.datetime(2018, 1, 1)
     main(run_real=run_real, send_orders_to_broker=True, start_date=start_date,
          conf_portfolio=conf_portfolio,asset_type=asset_type)
