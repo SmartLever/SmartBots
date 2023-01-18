0e4c7dc1ba2e4c410241cbdaad4cc6eecd46e199
andres <andres.carpio@smsdata.es>
2023-01-18 21:24:24 +0100

    update bug in portfolio_production

--- src/application/bots/portfolio_production.py
+++ src/application/bots/portfolio_production.py
@@ -27,7 +27,7 @@ def main(run_real: bool = False, send_orders_to_broker: bool = True,
     from src.application.services.portfolio_constructor import Portfolio_Constructor
     path = os.path.abspath(__file__)
     path_bot = os.path.dirname(path)  # path to the module
-    path_to_config = os.path.join(conf.path_to_data, "config_portfolios", conf_portfolio+".yaml")
+    path_to_config = os.path.join(conf.path_modulo, "application", "config_portfolios", conf_portfolio+".yaml")
     conf_portfolio = get_config(path_to_config)  # get the configuration from the config file
     # if our strategy is in a csv file
     list_strategy = []
@@ -60,7 +60,7 @@ def main(run_real: bool = False, send_orders_to_broker: bool = True,
 if __name__ == '__main__':
     run_real = True
     asset_type = 'financial'
-    conf_portfolio = 'config_financial'
+    conf_portfolio = 'config_webhook'
     start_date = dt.datetime(2018, 1, 1)
     main(run_real=run_real, send_orders_to_broker=True, start_date=start_date,
          conf_portfolio=conf_portfolio,asset_type=asset_type)
